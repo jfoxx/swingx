5ccf3fa58fac971613935b7a1b54bbb3ee821ba6
jack <jack.enqvist@gmail.com>
2013-12-17 23:26:30 +0100

    created a script for starting king of the hill gamemode.

---
+++ Assets/MultiPlayer/Scripts/Hill.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hill : MonoBehaviour {
+
+
+	private float interval = 1;
+	private float timer;
+	private GameObject manager;
+
+	void Start () 
+	{
+		timer = interval;
+		manager = GameObject.Find("Manager");
+	}
+	
+	void Update () 
+	{	
+		if(timer > 0){
+			timer -= Time.deltaTime;
+		}
+
+	}
+
+	void OnTriggerStay2D(Collider2D coll) 
+	{
+		if(timer <= 0 && coll.transform.CompareTag("player"))
+		{
+			updatePlayeScore(coll.networkView.owner);
+		}
+	}
+
+	void updatePlayeScore(NetworkPlayer player)
+	{
+		timer = interval;
+		manager.networkView.RPC("updatePlayerScore", RPCMode.AllBuffered, player);
+	}
+
+}
